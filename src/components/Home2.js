5dfb6ca37400a7c99167d5a0a5fce51392ab2f2f
Anja <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-08-06 13:38:26 +0200

    up

--- src/components/Home2.js
+++ src/components/Home2.js
@@ -11,7 +11,7 @@ const Home = () => {
 
   // API KEY
   //to fetch env variable from frontend it HAS TO be named REACT_APP_... -https://create-react-app.dev/docs/adding-custom-environment-variables/
-  const apiKey = process.env.REACT_APP_API_KEY;
+    const apiKey = process.env.REACT_APP_API_KEY;
   //console.log(apiKey);
 
     //State
@@ -23,29 +23,36 @@ const Home = () => {
     const [temp_max, setTemp_max] = useState('');
     // const [icon, setIcon] = useState();
     // const [isLoading, setLoading] = useState();
-    //const [error, setError] = useState(null);
+    // const [error, setError] = useState(false);
 
     const getWeatherData = async (event) => {
-      event.preventDefault(); //by default, on submission of a form, the page will redirect and refresh - we don't want that
-      
+      //by default, on submission of a form, the page will redirect and refresh - we don't want that
+      event.preventDefault();
+
+      //saving user input values in variables
+      /* e.currentTarget always refers to the element to which
+       the event handler has been attached, as opposed to event.target,
+      which identifies the element on which the event occurred and which may be its descendant.*/
+
       const city = event.target.elements.city.value;
       //const country = event.target.elements.country.value; //uncomment if input field for country is needed in WeatherForm
       
       if ( (city && country) || city ) { 
-            try { 
-                const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&mode=json&units=metric&appid=${apiKey}`);
-            
-                setCity(response.data.name);
-                setCountry(response.data.sys.country);
-                setTemp(response.data.main.temp);
-                setTemp_max(response.data.main.temp_max);
-                setTemp_min(response.data.main.temp_min);
-                setDescription(response.data.weather[0].description); //[0] - weather is an array so displaying only first element, doesn't work without it
-            } catch (err) {
-                console.log('Something went wrong! ' + err);
-                alert('Something went wrong! ' + err);
-            }
+        try { 
+          const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&mode=json&units=metric&appid=${apiKey}`);
+          
+          setCity(response.data.name);
+          setCountry(response.data.sys.country);
+          setTemp(response.data.main.temp);
+          setTemp_max(response.data.main.temp_max);
+          setTemp_min(response.data.main.temp_min);
+          setDescription(response.data.weather[0].description); //[0] - weather is an array so displaying only first element, doesn't work without it
+        } catch (err) {
+          console.log('Something went wrong! ' + err);
+          alert('Something went wrong! ' + err);
         }
+
+      }
     }
 
   return (
--- src/components/WeatherTable.js
+++ src/components/WeatherTable.js
@@ -3,44 +3,56 @@ import React from "react";
 //styles
 import Table from 'react-bootstrap/Table'
 
+/*const ErrorMessage = () => {
+    return (
+      <>
+        <span className='errMsg' style={{display:'block'}}>Sorry, something went wrong.</span>
+      </>
+    )
+}*/
+
 const Weather = //passing props:
     ({ 
-        city, 
+        city,
         country,
         temp,
         temp_max,
         temp_min,
         description,
     }) => {
-        return (
-            <>
-                <div className='weatherTable'>
-                    { (city && country) || city  ? <span className='cityCountry'><h3>{ city }, { country }</h3></span> : null }
-                    <Table striped bordered hover>
-                        <thead>
-                            <tr>
-                            <th>Temperature °C</th>
-                            <th>Min-Max °C</th>
-                            <th>Weather Description</th>
-                            </tr>
-                        </thead>
 
-                        <tbody>
-                            <tr>
-                            {/*Math.floor() function returns the largest integer less than or equal to a given number */}
-                            <td>{ temp ? <span>{ Math.floor(temp) }&deg;</span> : null }</td>
-                            <td>  { temp_min && temp_max 
-                                        ? <span>{ Math.floor(temp_min) }&deg; - { Math.floor(temp_max) }&deg;</span>
-                                        : null 
-                                    }
-                            </td>
-                            <td>{ description }</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </div>
-            </>
-        );
-}
+            return (
+                <>
+                    <div className='weatherTable'>
 
+                        { city  ? <span className='cityCountry'><h3>{ city }, { country }</h3></span> : null }
+                    
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                <th>Temperature °C</th>
+                                <th>Min-Max °C</th>
+                                <th>Weather Description</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                {/*Math.floor() function returns the largest integer less than or equal to a given number */}
+                                <td>{ temp ? <span>{ Math.floor(temp) }&deg;</span> : null }</td>
+                                <td>  { temp_min && temp_max 
+                                            ? <span>{ Math.floor(temp_min) }&deg; - { Math.floor(temp_max) }&deg;</span>
+                                            : null 
+                                        }
+                                </td>
+                                <td>{ description }</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </div>
+                </>
+            );
+            
+        };
+    
 export default Weather;
