9050859ac110365e90b9cfcb9082de7149ca9ded
Anja <73844750+tasteslikestrawberries@users.noreply.github.com>
2021-08-09 12:46:11 +0200

    finished auth0 login

--- src/App.js
+++ src/App.js
@@ -7,31 +7,45 @@ import {
   Link
 } from "react-router-dom";
 
-//styles
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button'
-import './styles.css';
+//services
+import { useAuth0 } from '@auth0/auth0-react';
 
 //components
 import Weather from './components/Weather/Weather'
 import UserProfile from './components/Login/UserProfile'
 import About from './components/About'
 
+//styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button'
+import './styles.css';
+
 const App = () => {
+  /*destructuring, this is fetching the loginWithRedirect that is a property of useAuth0() function 
+  that has many properties, but we only need this one, not the whole object that the function returns. 
+  Without destructuring it would look like:
+  const x = useAuth();
+  const loginWithRedirect = x.loginWithRedirect*/
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0(); 
 
   return (
     <Router>
       <>
         <nav>
               <ul>
+                { !isAuthenticated ?
                   <li>
-                    <Button  className='btn-lg' type='submit' value='search' variant='primary' style={{backgroundColor:'#0854c4', fontSize:'large'}}>Login</Button>
+                    <Button  onClick={() => loginWithRedirect()} className='btn-lg' type='submit' value='search' variant='primary' style={{backgroundColor:'#0854c4', fontSize:'large'}}>Log In</Button>
                   </li>
-
+                :
                   <li>
-                    <Link to='/'>Weather</Link>
+                    <Button  onClick={() => logout()} className='btn-lg' type='submit' value='search' variant='primary' style={{backgroundColor:'#0854c4', fontSize:'large'}}>Log Out</Button>
                   </li>
-
+                }
+                  <li>
+                    <Link to='/myprofile'>My Profile</Link>
+                  </li>
+               
                   <li> 
                     <Link to='/about'>About</Link>
                   </li>
--- src/components/Login/UserProfile.js
+++ src/components/Login/UserProfile.js
@@ -1,30 +1,43 @@
 import React from 'react';
 
+//services
+import { useAuth0 } from '@auth0/auth0-react';
+
 //styles
 import Button from 'react-bootstrap/Button'
 
-//icons
+/*//icons
 import { Icon } from '@iconify/react';
 import bxsUserCircle from '@iconify/icons-bx/bxs-user-circle';
+<Icon className='loginIcon' icon={bxsUserCircle} style={{color: '#0683f1', fontSize: '90px'}} />*/
 
 const UserProfile = () => {
+
+  const { user, loginWithRedirect, isAuthenticated } = useAuth0();
+
   return (
     <>
+    { isAuthenticated ?
       <div className='header'>
-        <Icon className='loginIcon' icon={bxsUserCircle} style={{color: '#0683f1', fontSize: '90px'}} />
-        <span><h1>My Profile</h1></span>
+        <img className='userLogo' src={user.picture} alt={user.name} />
+        <span><h1>{user.name}</h1></span>
+        <span><h3>(<i>{user.nickname})</i></h3></span>
+        <span><h4>{user.email}</h4></span>
       </div>
-
-      <div className='homeContainer'>
-
-        <div className="col text-center">
-          <Button className='button' type='submit' value='search' variant='primary' style={{marginTop: '1rem'}}>Logout</Button>
-        </div>
-      
+    : <div className='noUser'>
+      <h5>Please <Button  onClick={() => loginWithRedirect()}  type='submit' value='search' variant='primary' style={{fontSize:'large'}}>Log In</Button> to view this page.</h5>
       </div>
+    }
+
     </>
   )
  
 }
 
 export default UserProfile;
+
+   /*<div className='homeContainer'>
+        <pre className='userInfo'>
+        //{JSON.stringify(user, null, 2)}
+        </pre>
+      </div>*/
